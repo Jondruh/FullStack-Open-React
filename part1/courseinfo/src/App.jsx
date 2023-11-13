4e4e270925c3ca41dd13974759c1296b84b0ff6f
Jonathan Hurd <hurd.jonathan@gmail.com>
2023-11-13 14:59:16 -0600

    exercises 2.1 - 2.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,43 +1,54 @@
+import Course from "./components/Course"
+
 const App = () => {
-  const course = 'Half Stack application development'
-  const content = [
-    {key: 1, part: 'Fundamentals of React', exercises: 10},
-    {key: 2, part: 'Using props to pass data', exercises: 7},
-    {key: 3, part: 'State of a component', exercises: 14}
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
   ]
 
   return (
-    <div>
-      <Header course={course} />
-      <Content parts={content} />
-      <Total parts={content} />
-    </div>
+    courses.map(course => <Course key={course.id} course={course} />)
   )
-}
-
-const Header = (props) => {
-  return (
-    <h1>{props.course}</h1>
-  )
-}
-
-const Content = (props) => {
-  let toRender = [];
-  console.log(props)
-  props.parts.forEach(part => {
-    toRender.push(<li key={part.key}>{part.part} {part.exercises}</li>);
-  });
-
-  return toRender;
-
-}
-
-const Total = (props) => {
-  const total = props.parts
-    .map(part => part.exercises)
-    .reduce((a, b) => a + b);
-
-  return <p>Number of exercises: {total}</p>
 }
 
 export default App
