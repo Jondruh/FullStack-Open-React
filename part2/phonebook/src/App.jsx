e9d8e80ed52e6143f00fed0fe9de3285c170bc2e
Jonathan Hurd <hurd.jonathan@gmail.com>
2023-11-15 12:33:52 -0600

    exercises 2.16-2.17

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,6 +3,7 @@ import SearchBar from './components/SearchBar'
 import { Persons } from './components/Persons';
 import { PersonForm } from './components/PersonForm';
 import phoneService from './services/phoneService'
+import { Notification } from './components/Notification';
 
 const App = () => {
   const [persons, setPersons] = useState([])
@@ -10,6 +11,7 @@ const App = () => {
   const [newPhone, setNewPhone] = useState('');
   const [search, setNewSearch] = useState('');
   const [searchPersons, setNewSearchPersons] = useState([]);
+  const [message, setMessage] = useState(null);
 
   useEffect(() => {
     phoneService.getAll()
@@ -40,6 +42,7 @@ const App = () => {
 
   const deleteName = (id) => {
     const targetInd = persons.findIndex(person => person.id === id);
+    const person = persons[targetInd];
     if (window.confirm(`Delete ${persons[targetInd].name}?`)) {
       phoneService.remove(id)
         .then(response => {
@@ -47,6 +50,14 @@ const App = () => {
             const newPersons = persons.toSpliced(targetInd, 1);
             setPersons(newPersons);
             setNewSearchPersons(newPersons);
+            flashNotification(`${person.name} has been removed`);
+          } else {
+            flashNotification(`${person.name} has already been removed from server`, true)
+            phoneService.getAll()
+            .then(people => {
+              setPersons(people);
+              setNewSearchPersons(people);
+            });
           }
         })
     }
@@ -55,6 +66,7 @@ const App = () => {
   const updatePerson = (name, phone) => {
     const index = persons.findIndex(person => person.name === name);
     const id = persons[index].id;
+    const person = persons[targetInd];
     phoneService.update(id, { name, phone }).
       then(response => {
         if (response.status === 200) {
@@ -63,8 +75,24 @@ const App = () => {
           setNewSearchPersons(newPersons);
           setNewName('');
           setNewPhone('');
+          flashNotification(`${newName} has been updated`);
+        } else {
+          flashNotification(`${person.name} has already been removed from server`, true)
+          phoneService.getAll()
+          .then(people => {
+            setPersons(people);
+            setNewSearchPersons(people);
+          });
         }
       });
+  }
+
+  const flashNotification = (message, error) => {
+    setMessage({message, error});
+
+    setTimeout(() => {
+      setMessage(null);
+    }, 3000);
   }
 
   const addName = (event) => {
@@ -84,6 +112,7 @@ const App = () => {
           setNewSearchPersons(newPersons);
           setNewName('');
           setNewPhone('');
+          flashNotification(`${newName} has been added to the list`)
         })
     }
   }
@@ -91,6 +120,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification notification={message} />
       <SearchBar searchHandler={handleSearchChange} search={search} />
 
       <h2>Add New</h2>
